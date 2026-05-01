d162559920c6e4cf108a67196521afd6d72d8586
Vatsal Agrawal <vatsal.aggarwal@squadstack.com>
2026-05-02 01:34:02 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def pop(self, arr: list, idx: int, x: int) -> int:
+        arr[idx] -= 1
+
+        if arr[idx] == 0:
+            return x + 1
+        
+        if arr[idx] == -1:
+            return x - 1
+
+        return x
+
+    def push(self, arr: list, idx: int, x: int) -> int:
+        arr[idx] += 1
+        if arr[idx] == 1:
+            return x - 1
+        
+        if arr[idx] == 0:
+            return x + 1
+
+        return x
+
+    def get_idx(self, char: str) -> int:
+        return ord(char) - ord('a')
+        
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        x = 0
+        arr = [0] * 26
+
+        for char in s1:
+            idx = self.get_idx(char)
+            x = self.pop(arr, idx, x)
+
+        for i in range(len(s2)):
+            if i >= len(s1):
+                idx = self.get_idx(s2[i - len(s1)])
+                x = self.pop(arr, idx, x)
+
+            idx = self.get_idx(s2[i])
+            x = self.push(arr, idx, x)
+            if x == 0:
+                return True
+
+        return False
+        
