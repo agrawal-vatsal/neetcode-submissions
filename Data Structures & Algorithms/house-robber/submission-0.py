1081037313fe1da773233d1ee352cb9feb8045f1
Vatsal Agrawal <vtslaggarwal@gmail.com>
2026-05-14 03:10:36 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,19 @@
+[6, 2, 2, 5, 2, 5, 5]
+6, 2, 8, 11, 10, 16, 15
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        helper_arr = [nums[0], nums[1]]
+        max_till_now = nums[0]
+
+        for i in range(2, len(nums)):
+            helper_arr.append(max_till_now + nums[i])
+            max_till_now = max(max_till_now, helper_arr[-2])
+
+        return max(helper_arr)
